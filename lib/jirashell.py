0c68fd5bc97633f366cf6bb0b9eaf828ae6addb8
Trent Mick <trentm@gmail.com>
2012-08-07 16:53:58 -0700

    always log full traceback and request a ticket for any top-level exceptions

--- lib/jirashell.py
+++ lib/jirashell.py
@@ -817,7 +817,6 @@ def main(argv=sys.argv):
     return shell.main(argv, loop=cmdln.LOOP_IF_EMPTY)
 
 
-## {{{ http://code.activestate.com/recipes/577258/ (r5)
 if __name__ == "__main__":
     try:
         retval = main(sys.argv)
@@ -826,27 +825,17 @@ if __name__ == "__main__":
     except SystemExit:
         raise
     except:
-        import traceback, logging
-        if not log.handlers and not logging.root.handlers:
-            logging.basicConfig()
-        skip_it = False
-        exc_info = sys.exc_info()
-        if hasattr(exc_info[0], "__name__"):
-            exc_class, exc, tb = exc_info
-            if isinstance(exc, IOError) and exc.args[0] == 32:
-                # Skip 'IOError: [Errno 32] Broken pipe': often a cancelling of `less`.
-                skip_it = True
-            if not skip_it:
-                tb_path, tb_lineno, tb_func = traceback.extract_tb(tb)[-1][:3]
-                log.error("%s (%s:%s in %s)", exc_info[1], tb_path,
-                    tb_lineno, tb_func)
-        else:  # string exception
-            log.error(exc_info[0])
-        if not skip_it:
-            if log.isEnabledFor(logging.DEBUG):
-                print()
-                traceback.print_exception(*exc_info)
-            sys.exit(1)
+        import traceback
+        print()
+        traceback.print_exc()
+        print("""
+* * * * * * * * * * * * * * * * * * * * * * * *
+* Please log a bug at                         *
+*    https://github.com/trentm/jirash/issues  *
+* to report this error. Thanks!               *
+* -- Trent                                    *
+* * * * * * * * * * * * * * * * * * * * * * * *""")
+        sys.exit(1)
     else:
         sys.exit(retval)
 ## end of http://code.activestate.com/recipes/577258/ }}}
